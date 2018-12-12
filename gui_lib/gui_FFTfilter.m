7d24d00c8890916b5d5e3256a1c16b9373d4ebdc
Dmitry Tebaykin <dmitry.tebaykin@gmail.com>
2018-12-12 14:25:16 -0800

    Added comments, swapped reverse transform order

--- gui_lib/gui_FFTfilter.m
+++ gui_lib/gui_FFTfilter.m
@@ -1,18 +1,40 @@
 function [countsNoNoise] = gui_FFTfilter(counts, gauss_blur_radius, spectral_radius, scaling_factor)
-    im = counts*scaling_factor;
+    % Scale the image 
+    im = counts * scaling_factor;
+    
+    % Mean of all values
     mean_value = mean(im(:));
-    im = im-mean_value;
-    if gauss_blur_radius>0
+    
+    % Center the distribution
+    im = im - mean_value;
+    
+    % Add custom blur
+    if gauss_blur_radius > 0
         im = imgaussfilt(im, gauss_blur_radius);
     end
+    
+    % 2D FFT and shift low frequency components to the center
     F = fftshift(fft2(im));
+    
+    % Create a 2D gaussian filter
     h = fspecial('gaussian',size(F),spectral_radius);
+    
+    % Rescale to [0 1]
     h = h./max(h(:));
+    
+    % Apply the filter to the FFT data. Central components remain largely
+    % unaffected, outer edge data gets multiplied by low values
     H = F.*h;
+    
+    % Reverse FFT
     pic = real(ifft2(fftshift(H)));
-    pic(pic<0)=0;
+    
+    % Check how negative these get
+    pic(pic < 0) = 0;
+    
+    % Add mean intensity back to data
     pic = floor(pic + mean_value);
-    pic = sqrt(pic.*counts);
-    countsNoNoise = pic/scaling_factor;
+    
+    countsNoNoise = sqrt((pic/scaling_factor) .* counts);
 end
 
